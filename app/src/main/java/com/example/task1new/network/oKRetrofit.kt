0e86071c1464b713b252ea0b43eb062bea08310e
Aleksey Petrov <Alekseyandreevichpetrov304@gmail.com>
2021-07-14 18:49:59 +0300

    Starting Task4

--- app/src/main/java/com/example/task1new/network/oKRetrofit.kt
+++ app/src/main/java/com/example/task1new/network/oKRetrofit.kt
@@ -7,7 +7,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-
+//
 object OkRetrofit {
 
     private val loggingInterceptor = HttpLoggingInterceptor()
