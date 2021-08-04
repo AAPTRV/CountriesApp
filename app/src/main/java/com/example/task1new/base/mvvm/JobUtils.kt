b5c55e5348f7ac92b3b4838458cd37d1f9d25aeb
Aleksey Petrov <Alekseyandreevichpetrov304@gmail.com>
2021-08-04 12:20:02 +0300

    Doing MVVM (nothing special)

--- app/src/main/java/com/example/task1new/base/mvvm/JobUtils.kt
+++ app/src/main/java/com/example/task1new/base/mvvm/JobUtils.kt
@@ -1,30 +1,41 @@
 package com.example.task1new.base.mvvm
 
 import androidx.lifecycle.MutableLiveData
+import com.example.task1new.base.mvvm.Outcome.Companion.failure
 import com.example.task1new.base.mvvm.Outcome.Companion.loading
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.Disposable
 
-fun <T> executeJob(job: Flowable<T>, outcome: MutableLiveData<Outcome<T>>): Disposable{
+fun <T> executeJob(job: Flowable<T>, outcome: MutableLiveData<Outcome<T>>): Disposable {
     outcome.loading(true)
     return job.executeOnIoThread()
         .subscribe(
-            {outcome.next(it)}
+            { outcome.next(it) }, { outcome.failed(it) }
         )
 }
 
-fun <T> executeJob(job: Single<T>, outcome: MutableLiveData<Outcome<T>>): Disposable{
-
-}
-
-fun <T> MutableLiveData<Outcome<T>>.loading(isLoading: Boolean){
+fun <T> MutableLiveData<Outcome<T>>.loading(isLoading: Boolean) {
     this.value = Outcome.loading(isLoading)
 }
 
 fun <T> MutableLiveData<Outcome<T>>.success(t: T) {
-    with(this){
+    with(this) {
         loading(false)
         value = Outcome.success(t)
+    }
+}
+
+fun <T> MutableLiveData<Outcome<T>>.next(t: T) {
+    with(this) {
+        loading(false)
+        value = Outcome.next(t)
+    }
+}
+
+fun <T> MutableLiveData<Outcome<T>>.failed(e: Throwable) {
+    with(this) {
+        loading(false)
+        value = Outcome.failure(e)
     }
 }
