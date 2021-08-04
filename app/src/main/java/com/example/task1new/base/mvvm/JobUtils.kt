348a299507a64bfadab5de81b71ab15e476d08a4
Aleksey Petrov <Alekseyandreevichpetrov304@gmail.com>
2021-08-04 10:58:12 +0300

    Doing MVVM (nothing special)

---
+++ app/src/main/java/com/example/task1new/base/mvvm/JobUtils.kt
@@ -0,0 +1,13 @@
+package com.example.task1new.base.mvvm
+
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.Disposable
+
+fun <T> executeJob(job: Flowable<T>, outcome: MutableLiveData<Outcome<T>>): Disposable{
+}
+
+fun <T> executeJob(job: Single<T>, outcome: MutableLiveData<Outcome<T>>): Disposable{
+
+}
