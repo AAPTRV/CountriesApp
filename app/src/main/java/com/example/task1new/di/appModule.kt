f56a7381a62e1ad7b4f2c6f2d67f0b1d2f14fef4
Aleksey Petrov <Alekseyandreevichpetrov304@gmail.com>
2021-09-10 12:32:26 +0300

    Code refactoring

--- app/src/main/java/com/example/task1new/di/appModule.kt
+++ app/src/main/java/com/example/task1new/di/appModule.kt
@@ -1,15 +1,18 @@
 package com.example.task1new.di
 
+import com.example.data.network.CoroutinesCountryService
+import com.example.data.network.CountryService
 import com.example.data.repository.databaseRepo.DatabaseCommonInfoRepositoryImpl
 import com.example.data.repository.databaseRepo.DatabaseLanguageRepositoryImpl
 import com.example.data.repository.networkRepo.NetworkRepositoryCoroutinesImpl
-import com.example.domain.repository.network.NetworkRepository
-import com.example.task1new.Retrofit
 import com.example.data.repository.networkRepo.NetworkRepositoryImpl
 import com.example.domain.repository.database.DatabaseCommonInfoRepository
 import com.example.domain.repository.database.DatabaseLanguageRepository
+import com.example.domain.repository.network.NetworkRepository
 import com.example.domain.repository.network.NetworkRepositoryCoroutines
+import com.example.task1new.Retrofit
 import org.koin.dsl.module
+
 
 val appModule = module {
 
@@ -19,9 +22,9 @@ val appModule = module {
     single <CoroutinesCountryService>{ Retrofit.getCountriesCoroutinesApi() }
 
     //Data level
-    single <NetworkRepositoryCoroutines>{ NetworkRepositoryCoroutinesImpl(get(), get())}
-    single <NetworkRepository>{ NetworkRepositoryImpl(get(), get())}
-    single <DatabaseCommonInfoRepository>{ DatabaseCommonInfoRepositoryImpl(get())}
-    single <DatabaseLanguageRepository>{ DatabaseLanguageRepositoryImpl(get())}
+    single <NetworkRepositoryCoroutines>{ NetworkRepositoryCoroutinesImpl(get(), get()) }
+    single <NetworkRepository>{ NetworkRepositoryImpl(get(), get()) }
+    single <DatabaseCommonInfoRepository>{ DatabaseCommonInfoRepositoryImpl(get()) }
+    single <DatabaseLanguageRepository>{ DatabaseLanguageRepositoryImpl(get()) }
 
 }
